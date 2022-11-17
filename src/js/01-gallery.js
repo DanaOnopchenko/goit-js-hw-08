d1382808eb0d1a1fdfdf0ab24f422e4f2745ed19
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-11-17 04:08:11 +0200

    task 08-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import '../css/01-gallery.css';
+import '../css/common.css';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+// galleryEl.addEventListener('click', onOpenModal)
+const markup = galleryItems.map(({
+    preview,
+    original,
+    description
+}) => `
+<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+`).join('')
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: 250 });
