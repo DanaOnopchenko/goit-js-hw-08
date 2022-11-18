376f014e6d2e0e1927bcc96a80ddf20ae74199b8
DanaOnopchenko <dana.onopchenko@gmail.com>
2022-11-18 23:15:04 +0000

    Deploying to gh-pages from @ DanaOnopchenko/goit-js-hw-08@513c90f1cad3e6398b26c43961f320e17f79487c 🚀

---
+++ 03-feedback.004cdab7.js
@@ -0,0 +1,2 @@
+!function(){var o={form:document.querySelector(".feedback-form")};console.log(o.form);var e=document.querySelector(".feedback-form");console.log(e)}();
+//# sourceMappingURL=03-feedback.004cdab7.js.map
